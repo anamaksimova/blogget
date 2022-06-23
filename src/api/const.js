471046943de4cb665fd3ce35996e47f37a92b6ce
anamaksimova <19cherrytree91@gmail.com>
2022-06-23 09:26:15 +0300

    m5_l6

--- src/api/const.js
+++ src/api/const.js
@@ -1,5 +1,5 @@
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
-export const CLIENT_ID = '0N393qWrNM3qi6OaHfzo-A';
+export const CLIENT_ID = '';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
 export const REDIRECT_URI = 'http://localhost:3001/auth';
