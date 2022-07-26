b3fe6200e64cd02d3929c52e516dd12cf11b3d75
anamaksimova <19cherrytree91@gmail.com>
2022-07-26 11:15:13 +0300

    m5_l12

--- src/store/posts/postAction.js
+++ src/store/posts/postAction.js
@@ -1,68 +1,68 @@
 import axios from 'axios';
 import {URL_API} from '../../api/const';
 import {postsSlice} from './postsSlice';
-import {createAsyncThunk} from '@reduxjs/toolkit';
-// export const postRequestAsync = (newPage) => (dispatch, getState) => {
-//   let page = getState().postReducer.page;
-//   if (newPage) {
-//     page = newPage;
-//     dispatch(postsSlice.actions.changePage(page));
-//   }
-//   const token = getState().tokenReducer.token;
-//   const after = getState().postReducer.after;
-//   const loading = getState().postReducer.loading;
-//   const isLast = getState().postReducer.isLast;
-
-//   if (!token || loading || isLast) return;
-//   if (!after) {
-//     dispatch(postsSlice.actions.postsRequest());
-//   }
-//   axios(`${URL_API}/${page}?limit=10&${after ? `after=${after}` : ''}`, {
-//     headers: {
-//       Authorization: `bearer ${token}`,
-//     },
-//   }).then(({data}) => {
-//     if (after) {
-//       dispatch(postsSlice.actions.postsRequestSuccessAfter(data.data));
-//     } else {
-//       dispatch(postsSlice.actions.postsRequestSuccess(data.data));
-//     }
-//   })
-//     .catch(error => {
-//       dispatch(
-//  postsSlice.actions.postsRequestError({error: error.toString()}));
-//     });
-// };
-
-export const postRequestAsync = createAsyncThunk('posts/fetch',
-  (newPage, {getState}, {dispatch}) => {
-    let page = getState().postReducer.page;
-    if (newPage) {
-      page = newPage;
-      dispatch(postsSlice.actions.changePage(page));
-    }
-    const token = getState().tokenReducer.token;
-    const after = getState().postReducer.after;
-    const loading = getState().postReducer.loading;
-    const isLast = getState().postReducer.isLast;
-
-    if (!token || loading || isLast) return;
-    if (!after) {
-    // eslint-disable-next-line max-len
-      return axios(`${URL_API}/${page}?limit=10&${after ? `after=${after}` : ''}`,
-        {
-          headers: {
-            Authorization: `bearer ${token}`,
-          },
-        }).then(({data}) => {
-        if (after) {
-          return data.data;
-          // dispatch(postsSlice.actions.postsRequestSuccessAfter(data.data));
-        } else {
-          return data.data;
-        }
-      })
-        .catch(error => ({error: error.toString()}));
-    }
+// import {createAsyncThunk} from '@reduxjs/toolkit';
+export const postRequestAsync = (newPage) => (dispatch, getState) => {
+  let page = getState().postReducer.page;
+  if (newPage) {
+    page = newPage;
+    dispatch(postsSlice.actions.changePage(page));
   }
-);
+  const token = getState().tokenReducer.token;
+  const after = getState().postReducer.after;
+  const loading = getState().postReducer.loading;
+  const isLast = getState().postReducer.isLast;
+
+  if (!token || loading || isLast) return;
+  if (!after) {
+    dispatch(postsSlice.actions.postsRequest());
+  }
+  axios(`${URL_API}/${page}?limit=10&${after ? `after=${after}` : ''}`, {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  }).then(({data}) => {
+    if (after) {
+      dispatch(postsSlice.actions.postsRequestSuccessAfter(data.data));
+    } else {
+      dispatch(postsSlice.actions.postsRequestSuccess(data.data));
+    }
+  })
+    .catch(error => {
+      dispatch(postsSlice.actions.postsRequestError({error: error.toString()}));
+    });
+};
+
+// export const postRequestAsync = createAsyncThunk('posts/fetch',
+//   (newPage, {getState}, {dispatch}) => {
+//     let page = getState().postReducer.page;
+//     if (newPage) {
+//       page = newPage;
+//       dispatch(postsSlice.actions.changePage(page));
+//     }
+//     const token = getState().tokenReducer.token;
+//     const after = getState().postReducer.after;
+//     const loading = getState().postReducer.loading;
+//     const isLast = getState().postReducer.isLast;
+
+//     if (!token || loading || isLast) return;
+//     if (!after) {
+//       // eslint-disable-next-line max-len
+//       return
+//  axios(`${URL_API}/${page}?limit=10&${after ? `after=${after}` : ''}`,
+//         {
+//           headers: {
+//             Authorization: `bearer ${token}`,
+//           },
+//         }).then(({data}) => {
+//         if (after) {
+//           // return data.data;
+//           dispatch(postsSlice.actions.postsRequestSuccessAfter(data.data));
+//         } else {
+//           return data.data;
+//         }
+//       })
+//         .catch(error => ({error: error.toString()}));
+//     }
+//   }
+// );
--- src/store/posts/postsSlice.js
+++ src/store/posts/postsSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {postRequestAsync} from './postAction';
+// import {postRequestAsync} from './postAction';
 const initialState = {
   states: '',
   posts: [],
@@ -13,60 +13,26 @@ export const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    // // postsRequest: (state) => {
-    // //   state.error = '';
-    // //   state.states = 'loading';
-    // //   state.loading = true;
-    // // },
+    postsRequest: (state) => {
+      state.error = '';
+      state.states = 'loading';
+      state.loading = true;
+    },
     changePage: (state, action) => {
       state.states = 'loaded';
       state.page = action.payload;
       state.after = '';
       state.isLast = false;
     },
-    // postsRequestSuccessAfter: (state, action) => {
-    //   state.states = 'loaded';
-    //   state.posts = [...state.posts, ...action.payload.children];
-    //   state.error = '';
-    //   state.after = action.payload.after;
-    //   state.loading = false;
-    //   state.isLast = !action.payload.after;
-    // },
-    // postsRequestSuccess: (state, action) => {
-    //   state.posts = action.payload.children,
-    //   state.after = action.payload.after;
-    //   state.loading = false;
-    //   state.isLast = !action.payload.after;
-    //   state.states = 'loaded';
-    //   state.error = '';
-    // },
-    // postsRequestError: (state, action) => {
-    //   state.states = 'error';
-    //   state.error = action.error;
-    //   state.loading = false;
-    // },
-  },
-  extraReducers: {
-    [postRequestAsync.pending.type]: (state) => {
+    postsRequestSuccessAfter: (state, action) => {
+      state.states = 'loaded';
+      state.posts = [...state.posts, ...action.payload.children];
       state.error = '';
-      state.states = 'loading';
-      state.loading = true;
+      state.after = action.payload.after;
+      state.loading = false;
+      state.isLast = !action.payload.after;
     },
-    // changePage: (state, action) => {
-    //   state.states = 'loaded';
-    //   state.page = action.payload;
-    //   state.after = '';
-    //   state.isLast = false;
-    // },
-    // postsRequestSuccessAfter: (state, action) => {
-    // state.states = 'loaded';
-    // state.posts = [...state.posts, ...action.payload.children];
-    // state.error = '';
-    // state.after = action.payload.after;
-    // state.loading = false;
-    // state.isLast = !action.payload.after;
-    // },
-    [postRequestAsync.fulfilled.type]: (state, action) => {
+    postsRequestSuccess: (state, action) => {
       state.posts = action.payload.children,
       state.after = action.payload.after;
       state.loading = false;
@@ -74,11 +40,45 @@ export const postsSlice = createSlice({
       state.states = 'loaded';
       state.error = '';
     },
-    [postRequestAsync.rejected.type]: (state, action) => {
+    postsRequestError: (state, action) => {
       state.states = 'error';
       state.error = action.error;
       state.loading = false;
     },
-  }
+  },
+//   extraReducers: {
+//     [postRequestAsync.pending.type]: (state) => {
+//       state.error = '';
+//       state.states = 'loading';
+//       state.loading = true;
+//     },
+// changePage: (state, action) => {
+//   state.states = 'loaded';
+//   state.page = action.payload;
+//   state.after = '';
+//   state.isLast = false;
+// },
+// postsRequestSuccessAfter: (state, action) => {
+// state.states = 'loaded';
+// state.posts = [...state.posts, ...action.payload.children];
+// state.error = '';
+// state.after = action.payload.after;
+// state.loading = false;
+// state.isLast = !action.payload.after;
+// },
+//     [postRequestAsync.fulfilled.type]: (state, action) => {
+//       state.posts = action.payload.children,
+//       state.after = action.payload.after;
+//       state.loading = false;
+//       state.isLast = !action.payload.after;
+//       state.states = 'loaded';
+//       state.error = '';
+//     },
+//     [postRequestAsync.rejected.type]: (state, action) => {
+//       state.states = 'error';
+//       state.error = action.error;
+//       state.loading = false;
+//     },
+//   }
 });
 export default postsSlice.reducer;
