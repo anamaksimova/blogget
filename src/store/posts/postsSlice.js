eb0d347da4b96bec8d3ede12f3afca080303d2a8
anamaksimova <19cherrytree91@gmail.com>
2022-08-01 21:27:25 +0300

    m5_l12-5

--- src/store/posts/postsSlice.js
+++ src/store/posts/postsSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
-// import {postRequestAsync} from './postAction';
+import {postRequestAsync} from './postAction';
 const initialState = {
   states: '',
   posts: [],
@@ -13,72 +13,74 @@ export const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postsRequest: (state) => {
+    // postsRequest: (state) => {
+    //   state.error = '';
+    //   state.states = 'loading';
+    //   state.loading = true;
+    // },
+    // changePage: (state, action) => {
+    //   state.states = 'loaded';
+    //   state.page = action.payload;
+    //   state.after = '';
+    //   state.isLast = false;
+    // },
+    // postsRequestSuccessAfter: (state, action) => {
+    //   state.states = 'loaded';
+    //   state.posts = [...state.posts, ...action.payload.children];
+    //   state.error = '';
+    //   state.after = action.payload.after;
+    //   state.loading = false;
+    //   state.isLast = !action.payload.after;
+    // },
+    // postsRequestSuccess: (state, action) => {
+    //   state.posts = action.payload.children,
+    //   state.after = action.payload.after;
+    //   state.loading = false;
+    //   state.isLast = !action.payload.after;
+    //   state.states = 'loaded';
+    //   state.error = '';
+    // },
+    // postsRequestError: (state, action) => {
+    //   state.states = 'error';
+    //   state.error = action.error;
+    //   state.loading = false;
+    // },
+  },
+  extraReducers: {
+    [postRequestAsync.pending.type]: (state) => {
       state.error = '';
       state.states = 'loading';
       state.loading = true;
     },
-    changePage: (state, action) => {
-      state.states = 'loaded';
-      state.page = action.payload;
-      state.after = '';
-      state.isLast = false;
-    },
-    postsRequestSuccessAfter: (state, action) => {
-      state.states = 'loaded';
-      state.posts = [...state.posts, ...action.payload.children];
-      state.error = '';
+    // changePage: (state, action) => {
+    //   state.states = 'loaded';
+    //   state.page = action.payload;
+    //   state.after = '';
+    //   state.isLast = false;
+    // },
+    // postsRequestSuccessAfter: (state, action) => {
+    //   state.states = 'loaded';
+    //   state.posts = [...state.posts, ...action.payload.children];
+    //   state.error = '';
+    //   state.after = action.payload.after;
+    //   state.loading = false;
+    //   state.isLast = !action.payload.after;
+    // },
+    [postRequestAsync.fulfilled.type]: (state, action) => {
+      state.posts = action.payload.children;
       state.after = action.payload.after;
       state.loading = false;
       state.isLast = !action.payload.after;
-    },
-    postsRequestSuccess: (state, action) => {
-      state.posts = action.payload.children,
-      state.after = action.payload.after;
-      state.loading = false;
-      state.isLast = !action.payload.after;
       state.states = 'loaded';
       state.error = '';
+    //   state.page;
     },
-    postsRequestError: (state, action) => {
+    [postRequestAsync.rejected.type]: (state, action) => {
       state.states = 'error';
       state.error = action.error;
       state.loading = false;
     },
-  },
-//   extraReducers: {
-//     [postRequestAsync.pending.type]: (state) => {
-//       state.error = '';
-//       state.states = 'loading';
-//       state.loading = true;
-//     },
-// changePage: (state, action) => {
-//   state.states = 'loaded';
-//   state.page = action.payload;
-//   state.after = '';
-//   state.isLast = false;
-// },
-// postsRequestSuccessAfter: (state, action) => {
-// state.states = 'loaded';
-// state.posts = [...state.posts, ...action.payload.children];
-// state.error = '';
-// state.after = action.payload.after;
-// state.loading = false;
-// state.isLast = !action.payload.after;
-// },
-//     [postRequestAsync.fulfilled.type]: (state, action) => {
-//       state.posts = action.payload.children,
-//       state.after = action.payload.after;
-//       state.loading = false;
-//       state.isLast = !action.payload.after;
-//       state.states = 'loaded';
-//       state.error = '';
-//     },
-//     [postRequestAsync.rejected.type]: (state, action) => {
-//       state.states = 'error';
-//       state.error = action.error;
-//       state.loading = false;
-//     },
-//   }
+  }
 });
+console.log(postsSlice);
 export default postsSlice.reducer;
