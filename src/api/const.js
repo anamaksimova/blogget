ec6289caf42c785e0a49b84cd95bdc6df019d3dd
anamaksimova <19cherrytree91@gmail.com>
2022-08-10 02:40:28 +0300

    fix const redirect

--- src/api/const.js
+++ src/api/const.js
@@ -2,6 +2,6 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '0N393qWrNM3qi6OaHfzo-A';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3001/auth';
+export const REDIRECT_URI = 'https://blogget-seven.vercel.app/auth';
 export const SCOPE = 'identity submit read';
 export const URL_API = 'https://oauth.reddit.com';
